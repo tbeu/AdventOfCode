d36889fe84e5e49670fd16d1163c558ad8c6075f
tbeu <tc@tbeu.de>
2023-12-07 22:05:33 +0100

    Reuse count

--- 2023/day07/day07.cpp
+++ 2023/day07/day07.cpp
@@ -47,24 +47,24 @@ const auto cmpTie = [](const std::string& a, const std::string& b, const Cards& 
     return true;
 };
 
+const auto count = [](const std::string& s, const Cards& cards) {
+    Count sc{0, 0, 0, 0, 0};
+    for (const auto card : cards) {
+        if (const auto c = std::count(s.begin(), s.begin() + NCARDS, card); c > 0) {
+            sc[NCARDS - c]++;
+        }
+    }
+    assert(5 * sc[0] + 4 * sc[1] + 3 * sc[2] + 2 * sc[3] + sc[4] == NCARDS);
+    return sc;
+};
+
 struct
 {
     constexpr static inline Cards cards = {'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'};
     bool operator()(const std::string& a, const std::string& b) const
     {
-        const auto count = [](const std::string& s) {
-            Count sc{0, 0, 0, 0, 0};
-            for (const auto card : cards) {
-                if (const auto c = std::count(s.begin(), s.begin() + NCARDS, card); c > 0) {
-                    sc[NCARDS - c]++;
-                }
-            }
-            assert(5 * sc[0] + 4 * sc[1] + 3 * sc[2] + 2 * sc[3] + sc[4] == NCARDS);
-            return sc;
-        };
-
-        const auto ac = count(a);
-        const auto bc = count(b);
+        const auto ac = count(a, cards);
+        const auto bc = count(b, cards);
         if (ac < bc) {
             return true;
         }
@@ -85,10 +85,10 @@ struct
         const auto applyJoker = [](const std::string& s, Count& sc) {
             if (const auto csJ = std::count(s.begin(), s.begin() + NCARDS, Joker); csJ > 0) {
                 if (NCARDS == csJ) {
-                    assert(std::all_of(sc.begin(), sc.end(), [](auto i) { return i == 0; }));
-                    sc[0] = 1;  // s = "JJJJJ"
+                    assert(sc[0] == 1 && sc[1] == 0 && sc[2] == 0 && sc[3] == 0 && sc[4] == 0);
                     return;
                 }
+                sc[5 - csJ]--;
                 for (size_t i = 0; i < NCARDS; ++i) {
                     if (sc[i] > 0) {
                         sc[i]--;
@@ -96,26 +96,14 @@ struct
                         break;
                     }
                 }
+                assert(5 * sc[0] + 4 * sc[1] + 3 * sc[2] + 2 * sc[3] + sc[4] == NCARDS);
             }
         };
 
-        const auto count = [&applyJoker](const std::string& s) {
-            Count sc{0, 0, 0, 0, 0};
-            for (const auto card : cards) {
-                if (Joker == card) {
-                    continue;
-                }
-                if (const auto c = std::count(s.begin(), s.begin() + NCARDS, card); c > 0) {
-                    sc[NCARDS - c]++;
-                }
-            }
-            applyJoker(s, sc);
-            assert(5 * sc[0] + 4 * sc[1] + 3 * sc[2] + 2 * sc[3] + sc[4] == NCARDS);
-            return sc;
-        };
-
-        const auto ac = count(a);
-        const auto bc = count(b);
+        auto ac = count(a, cards);
+        applyJoker(a, ac);
+        auto bc = count(b, cards);
+        applyJoker(b, bc);
         if (ac < bc) {
             return true;
         }
