10365ca6b44b369f01cbd6f17248d341680fbb10
tbeu <tc@tbeu.de>
2023-12-11 21:59:41 +0100

    Modularize

--- 2023/day10/day10.cpp
+++ 2023/day10/day10.cpp
@@ -35,8 +35,85 @@ static bool readFile(const std::string& fileName, std::vector<std::string>& line
 constexpr auto verbose{false};
 using Pos = std::array<int16_t, 2>;
 using Pipes = std::set<std::pair<Pos, Pos> >;
+constexpr auto adjs = std::array<Pos, 4>{{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}};
 
-size_t bfs(const Pipes& pipes, std::vector<std::string>& map)
+static Pipes findLoop(const std::vector<std::string>& map, const Pos& start, uint8_t& dir)
+{
+    Pipes loop{};
+    const auto add = [&](const Pos& src, const Pos& dst) {
+        // ensure first < second for sorted insertion
+        if (0 == dir || 1 == dir) {
+            assert(src < dst);
+            loop.insert({src, dst});
+        } else {
+            assert(dst < src);
+            loop.insert({dst, src});
+        }
+    };
+    Pos pos = {start[0] + adjs[dir][0], start[1] + adjs[dir][1]};
+    auto& [r, c] = pos;
+    add(start, pos);
+    while (pos != start) {
+        auto src = pos;
+        if (0 == dir) {  // E
+            if ('-' == map[r][c]) {
+                c++;
+            } else if ('7' == map[r][c]) {
+                r++;
+                dir = 1;
+            } else if ('J' == map[r][c]) {
+                r--;
+                dir = 3;
+            } else {
+                loop.clear();
+                break;
+            }
+        } else if (1 == dir) {  // S
+            if ('|' == map[r][c]) {
+                r++;
+            } else if ('J' == map[r][c]) {
+                c--;
+                dir = 2;
+            } else if ('L' == map[r][c]) {
+                c++;
+                dir = 0;
+            } else {
+                loop.clear();
+                break;
+            }
+        } else if (2 == dir) {  // W
+            if ('-' == map[r][c]) {
+                c--;
+            } else if ('L' == map[r][c]) {
+                r--;
+                dir = 3;
+            } else if ('F' == map[r][c]) {
+                r++;
+                dir = 1;
+            } else {
+                loop.clear();
+                break;
+            }
+        } else if (3 == dir) {  // N
+            if ('|' == map[r][c]) {
+                r--;
+            } else if ('F' == map[r][c]) {
+                c++;
+                dir = 0;
+            } else if ('7' == map[r][c]) {
+                c--;
+                dir = 2;
+            } else {
+                loop.clear();
+                break;
+            }
+        }
+        add(src, pos);
+    }
+    return loop;
+}
+
+static size_t bfs(const Pipes& pipes, const std::vector<std::string>& map)
 {
     const Pos dim{static_cast<int16_t>(map.size()), static_cast<int16_t>(map[0].size())};
     std::vector<std::vector<bool> > visited(map.size(), std::vector<bool>(map[0].size(), false));
@@ -148,23 +225,22 @@ int main(int argc, char* argv[])
     map.emplace_back(std::string(dim[1], '.'));
     std::transform(map.begin(), map.end(), map.begin(), [](auto& row) { return '.' + std::move(row) + '.'; });
 
-    const auto getStart = [&]() -> Pos {
-        for (int16_t r = 1; r < dim[0] - 1; ++r) {
-            for (int16_t c = 1; c < dim[1] - 1; ++c) {
-                if ('S' == map[r][c]) {
-                    return {r, c};
-                }
-            }
-        }
-        assert(false);
-        return {0, 0};
-    };
-
-    const auto start = getStart();
     Pipes pipes;
+
     {
         // Part 1
-        constexpr auto adjs = std::array<Pos, 4>{{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}};
+        const auto getStart = [&]() -> Pos {
+            for (int16_t r = 1; r < dim[0] - 1; ++r) {
+                for (int16_t c = 1; c < dim[1] - 1; ++c) {
+                    if ('S' == map[r][c]) {
+                        return {r, c};
+                    }
+                }
+            }
+            assert(false);
+            return {0, 0};
+        };
+        const auto start = getStart();
         size_t max{};
         std::vector<uint8_t> dirs;
         for (uint8_t i = 0; i < adjs.size(); ++i) {
@@ -173,87 +249,11 @@ int main(int argc, char* argv[])
                 continue;
             }
             dirs.push_back(dir);
-            Pos pos = {start[0] + adjs[i][0], start[1] + adjs[i][1]};
-            auto& [r, c] = pos;
-            size_t steps{1};
-            Pipes loop{};
-            // ensure first < second for sorted insertion
-            if (0 == dir || 1 == dir) {
-                assert(start < pos);
-                loop.insert({start, pos});
-            } else {
-                assert(pos < start);
-                loop.insert({pos, start});
-            }
-            while (pos != start) {
-                auto src = pos;
-                if (0 == dir) {  // E
-                    if ('-' == map[r][c]) {
-                        c++;
-                    } else if ('7' == map[r][c]) {
-                        r++;
-                        dir = 1;
-                    } else if ('J' == map[r][c]) {
-                        r--;
-                        dir = 3;
-                    } else {
-                        steps = 0;
-                        break;
-                    }
-                } else if (1 == dir) {  // S
-                    if ('|' == map[r][c]) {
-                        r++;
-                    } else if ('J' == map[r][c]) {
-                        c--;
-                        dir = 2;
-                    } else if ('L' == map[r][c]) {
-                        c++;
-                        dir = 0;
-                    } else {
-                        steps = 0;
-                        break;
-                    }
-                } else if (2 == dir) {  // W
-                    if ('-' == map[r][c]) {
-                        c--;
-                    } else if ('L' == map[r][c]) {
-                        r--;
-                        dir = 3;
-                    } else if ('F' == map[r][c]) {
-                        r++;
-                        dir = 1;
-                    } else {
-                        steps = 0;
-                        break;
-                    }
-                } else if (3 == dir) {  // N
-                    if ('|' == map[r][c]) {
-                        r--;
-                    } else if ('F' == map[r][c]) {
-                        c++;
-                        dir = 0;
-                    } else if ('7' == map[r][c]) {
-                        c--;
-                        dir = 2;
-                    } else {
-                        steps = 0;
-                        break;
-                    }
-                }
-                steps++;
-                // ensure first < second for sorted insertion
-                if (0 == dir || 1 == dir) {
-                    assert(src < pos);
-                    loop.insert({src, pos});
-                } else {
-                    assert(pos < src);
-                    loop.insert({pos, src});
-                }
-            }
-            if (const auto s2 = steps / 2; s2 > 0) {
+            auto loop = findLoop(map, start, dir);
+            if (const size_t len2 = loop.size() / 2; len2 > 0) {
                 dirs.push_back((dir + 2) % 4);
-                if (s2 > max) {
-                    max = s2;
+                if (len2 > max) {
+                    max = len2;
                     pipes = std::move(loop);
                 }
             }
